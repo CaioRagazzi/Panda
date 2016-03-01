654e4d2f17a7e5c6783eab461f4cf5a10371ab84
CaioRagazzi <ca.ragazzi@gmail.com>
2016-03-01 11:16:40 -0300

    Add MasterPage

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class _Default : System.Web.UI.Page
+public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
     }
+
     protected void Button1_Click(object sender, EventArgs e)
     {
         FormsAuthentication.SignOut();
